40d28e0badf1351cbbb4e7a02b635cbdd0bbddff
Muhammad Reza Nur Rifqi <reza.rifqi@yahoo.com>
2023-11-10 17:13:04 +0700

    feat: add new font

--- src/components/ExperienceItem.tsx
+++ src/components/ExperienceItem.tsx
@@ -2,7 +2,7 @@ import { ExperienceItemProp } from "@/models/experience";
 
 export default function ExperienceItemComponent({ startYear, endYear, position, company, description, link, skills }: ExperienceItemProp) {
     return (
-        <div className="pt-10 grid grid-cols-6">
+        <div className="py-10 px-10 grid grid-cols-6 hover:bg-blue-400 hover:bg-opacity-20 hover:backdrop-blur-lg drop-shadow-lg rounded-md cursor-pointer">
             <div className="col-span-1 text-slate-500 text-xs font-semibold flex flex-col justify-start pt-1 w-32">
                 { startYear } - { endYear }
             </div>
--- src/components/RightSide.tsx
+++ src/components/RightSide.tsx
@@ -4,7 +4,7 @@ import ExperienceItemComponent from "./ExperienceItem";
 export default function RightSideComponent() {
     return (
         <main className="w-7/12 h-screen overflow-y-auto flex flex-col scrollbar-hide">
-            <p className="w-full pt-24">
+            <p className="w-full pt-24 mb-10">
                 I Started my journey as Software Engineer at 2012. My Journey started from company in my city Bandung.
             </p>
             {ExperienceDatas.map((exp) => (
