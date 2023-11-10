b9b84272403885cd1c1e33bcbc696dc53ae5fb8e
Muhammad Reza Nur Rifqi <reza.rifqi@yahoo.com>
2023-11-11 05:45:47 +0700

    fix: add key to item iter

--- src/components/SkillItem.tsx
+++ src/components/SkillItem.tsx
@@ -4,7 +4,7 @@ export default function SkillItemComponent({ skills }: SkillItemProp) {
     return (
         <div className="flex flex-row justify-start items-center gap-2 mt-2">
             {skills.map((skill, idx) => (
-                <div className="flex items-center bg-emerald-800/50 text-emerald-400 px-2 py-1 rounded-full text-xs font-semibold align-middle">{skill}</div>
+                <div className="flex items-center bg-emerald-800/50 text-emerald-400 px-2 py-1 rounded-full text-xs font-semibold align-middle" key={idx}>{skill}</div>
             ))}
         </div>
     )
