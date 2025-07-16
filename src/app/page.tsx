c76b4d518eef09481ef26fbfb418931e2af55613
Muhammad Reza Nur Rifqi <reza.rifqi@yahoo.com>
2025-07-16 17:18:51 +0700

    disable eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import ProjectCard from "../components/ProjectCard";
